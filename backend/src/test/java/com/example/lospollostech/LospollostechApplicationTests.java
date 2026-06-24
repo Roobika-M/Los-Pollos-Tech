6b25f678abf3d19680d40132e3f98375b9b2e4d7
Roobika <153609390+Roobika-M@users.noreply.github.com>
2026-06-24 10:50:05 +0530

    generated springboot

---
+++ backend/src/test/java/com/example/lospollostech/LospollostechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lospollostech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LospollostechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
