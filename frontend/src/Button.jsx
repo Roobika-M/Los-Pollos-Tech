affa016b7293d4ee53ae2780924256542ff7fe7c
Roobika <153609390+Roobika-M@users.noreply.github.com>
2026-06-29 09:37:05 +0530

    style changes

--- frontend/src/Button.jsx
+++ frontend/src/Button.jsx
@@ -1,8 +1,12 @@
+const name1 = "walt jr"
 function Button({ onClick }) {
   return (
+    <div className = "walt">
+    <h3>{name1}</h3>
     <button onClick={onClick}>
       Click Me
     </button>
+    </div>
   );
 }
 
