d2fceb4028981f2c9a7cf658655dfe95b410a988
Roobika <153609390+Roobika-M@users.noreply.github.com>
2026-07-01 17:50:35 +0530

    update: endpoints added

--- backend/src/main/java/com/lospollostech/backend/LosController.java
+++ backend/src/main/java/com/lospollostech/backend/LosController.java
@@ -1,6 +1,12 @@
 package com.lospollostech.backend;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,4 +24,25 @@ public class LosController {
     public LosEntity add(@RequestBody LosEntity reaction){
         return losService.addReaction(reaction);
     }
+
+    @GetMapping("/get")
+    public List<LosEntity> getAll(){
+        return losService.getReaction();
+    }
+
+    @GetMapping("/{id}")
+    public LosEntity getElementById(@PathVariable Long id){
+        return losService.getById(id);
+    }
+    
+    @PutMapping("/update/{id}")
+    public LosEntity update(@PathVariable Long id, @RequestBody LosEntity reaction){
+        return losService.updateReaction(id,reaction);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable Long id){
+        losService.deleteReaction(id);
+    }
+
 }
