eb919304358f82edef86568aa04d8590fe16747e
Roobika <153609390+Roobika-M@users.noreply.github.com>
2026-07-01 17:50:02 +0530

    update: service methods

--- backend/src/main/java/com/lospollostech/backend/LosService.java
+++ backend/src/main/java/com/lospollostech/backend/LosService.java
@@ -1,5 +1,7 @@
 package com.lospollostech.backend;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,5 +14,28 @@ public class LosService {
     }
     public LosEntity addReaction(LosEntity reaction){
         return losRepo.save(reaction);
+    }
+    public List<LosEntity> getReaction(){
+        return losRepo.findAll();
+    }
+    public LosEntity getById(Long id){
+        return losRepo.findById(id).orElse(null);
+    }
+    public LosEntity updateReaction(Long id, LosEntity reaction) {
+        LosEntity existingReaction = losRepo.findById(id).orElse(null);
+        if (existingReaction != null) {
+            existingReaction.setReactantOne(reaction.getReactantOne());
+            existingReaction.setReactantTwo(reaction.getReactantTwo());
+            existingReaction.setProduct(reaction.getProduct());
+            existingReaction.setReactionType(reaction.getReactionType());
+            existingReaction.setConditions(reaction.getConditions());
+            existingReaction.setDescription(reaction.getDescription());
+
+            return losRepo.save(existingReaction);
+        }
+        return null;
+    }
+    public void deleteReaction(Long id) {
+        losRepo.deleteById(id);
     }
 }
