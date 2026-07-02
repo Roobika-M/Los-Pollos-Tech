f8e2cb547104fe7a70fe90e0805e49108864e92f
Roobika <153609390+Roobika-M@users.noreply.github.com>
2026-07-02 18:35:02 +0530

    routes - added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Landing from "./pages/Landing";
+import Home from "./pages/Home";
+import Mix from "./pages/Mix";
+import AddReaction from "./pages/AddReaction";
+import ViewReactions from "./pages/ViewReactions";
+
 function App() {
-  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/mix" element={<Mix />} />
+        <Route path="/add" element={<AddReaction />} />
+        <Route path="/view" element={<ViewReactions />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
