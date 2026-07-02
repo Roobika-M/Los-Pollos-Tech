4534fce89eb839df59c34ca1bf813965e74aff52
Roobika <153609390+Roobika-M@users.noreply.github.com>
2026-07-02 11:33:29 +0530

    stable backend

--- backend/src/main/java/com/lospollostech/backend/LosController.java
+++ backend/src/main/java/com/lospollostech/backend/LosController.java
@@ -3,6 +3,7 @@ package com.lospollostech.backend;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/los")
 public class LosController {
